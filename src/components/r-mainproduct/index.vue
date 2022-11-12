0b460fe418bac6a916c284dcbcda070d05b26bcb
newwf <1399117902@qq.com>
2022-11-12 09:17:14 +0800

    11-12-dev

---
+++ src/components/r-mainproduct/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="r-mainproduct-components">
+    <!--商品图片-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'rmainproduct'
+}
+</script>
+
+<style scoped>
+
+</style>
