dfba7f9bd3138bf1e4b2ef7a1b978d4d4a5cb503
wenfu <wenfu814@163.com>
2023-01-04 15:50:46 +0800

    写了些样式

---
+++ dist/js/chunk-2d0b5e12.d4cab873.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b5e12"],{"1b7b":function(e,c,n){"use strict";n.r(c);var t=n("7a23");const a={class:"cart-page"},o=Object(t["createElementVNode"])("div",{class:"container"},"购物车内容界面",-1),r=[o];function s(e,c,n,o,s,d){return Object(t["openBlock"])(),Object(t["createElementBlock"])("div",a,r)}var d={name:"cartpage"},b=n("d959"),i=n.n(b);const p=i()(d,[["render",s]]);c["default"]=p}}]);
+//# sourceMappingURL=chunk-2d0b5e12.d4cab873.js.map
