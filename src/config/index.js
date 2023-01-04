cded623fba238bf804819fc021beaa68945f9cae
wenfu <wenfu814@163.com>
2023-01-04 16:00:18 +0800

    delete

--- src/config/index.js
+++
@@ -1,6 +0,0 @@
-// 请求地址
-const baseURL = 'https://apipc-xiaotuxian-front.itheima.net'
-
-export {
-  baseURL
-}
--- src/utils/storage.js
+++
@@ -1,7 +0,0 @@
-function getLocalStorageData (key) {
-  return JSON.parse(window.localStorage.getItem(key))
-}
-
-export {
-  getLocalStorageData
-}
